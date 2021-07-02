97dad8b1d0c0449de080fa30fdee64762aef0d72
Eko Cahyanto <49233072+cahya93@users.noreply.github.com>
2021-07-02 10:29:39 +0700

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,9 @@
+class BinarySearch(int arr[], int 1, int r, int x) {
+  
+  if(r >= 1) {                    //jiika r lebih dari sama dengan 1 maka jalankan code dibawahnya
+    int mid = 1 + (r - 1) /2;     //
+    
+    if(arr(mid) == x)             //jika arr dari mid sama dengan x maka kembalikan data ke mid
+      return mid;
+  }
+}
